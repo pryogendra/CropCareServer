87e3d67ec2f2bdad4c7c5d81b2857624943c6949
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2025-03-10 19:54:23 +0530

    Update wsgi.py

--- CropCare/wsgi.py
+++ CropCare/wsgi.py
@@ -10,10 +10,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CropCare.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(settings.BASE_DIR, 'staticfiles'))
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 #application = Cling(get_wsgi_application())
