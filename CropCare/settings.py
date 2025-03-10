6961cf3b20eb6235795599eb235147450081748b
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2025-03-10 19:13:43 +0530

    settings.py

--- CropCare/settings.py
+++ CropCare/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-&t#yk6gtrk##f5!qnjmc!=fs9oqjs=zm0qo_k4#8wfqftv-ujs
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
