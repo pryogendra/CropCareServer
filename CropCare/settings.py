4801669f8d1c17428daab249627806d96dbb843c
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2025-03-24 13:50:36 +0530

    Update settings.py

--- CropCare/settings.py
+++ CropCare/settings.py
@@ -139,6 +139,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "thecropcare.team@gmail.com"
-EMAIL_HOST_PASSWORD = "zggkqkpjzmpbgdzq"
+EMAIL_HOST_PASSWORD = "xxxxxxxxxxxxxxxx" #EMAIL HOST PASSWORD
 # EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 # EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
